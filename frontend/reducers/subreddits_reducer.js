004fcb00c0c136c9fa1a4bc4fd9db37702e04503
holyoke <jasonebueng@gmail.com>
2017-04-05 21:34:56 -0700

    adds subreddits and posts reducers

---
+++ frontend/reducers/subreddits_reducer.js
@@ -0,0 +1,10 @@
+import { RECEIVE_SUBREDDITS } from '../actions/subreddit_actions'
+
+const SubredditsReducer = (state = [], action) => {
+  switch (action.type) {
+    case RECEIVE_SUBREDDITS:
+      return action.subreddits
+    default:
+      return state
+  }
+}
